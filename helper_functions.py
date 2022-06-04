25bbb5b88900b24316d02de820573e42823f9c04
Swagat Kumar <swagat.kumar@gmail.com>
2022-06-04 12:22:24 +0100

    updated helper_functions

--- helper_functions.py
+++ helper_functions.py
@@ -239,3 +239,37 @@ def unzip_data(filename: str) -> None:
     zip_ref.close()
 
 
+
+def compare_histories(original_history, new_history, initial_epochs=5):
+  """
+  Comparing two tensorflow histories
+  """
+  acc = original_history.history['accuracy']
+  loss = original_history.history['loss']
+  val_acc = original_history.history['val_accuracy']
+  val_loss = original_history.history['val_loss']
+
+  # combine original history metrics with new_history metrics
+  total_acc = acc + new_history.history['accuracy']
+  total_loss = loss + new_history.history['loss']
+  total_val_acc = val_acc + new_history.history['val_accuracy']
+  total_val_loss = val_loss + new_history.history['val_loss']
+
+  # make plots
+  plt.figure(figsize=(8,8))
+  plt.subplot(2,1,1)
+  plt.plot(total_acc, label='training_accuracy')
+  plt.plot(total_val_acc, label='validation_accuracy')
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label='Start Fine Tuning')
+  plt.legend(loc='lower right')
+  plt.title('Training & Validation Accuracy')
+
+  plt.figure(figsize=(8,8))
+  plt.subplot(2,1,2)
+  plt.plot(total_loss, label='training_accuracy')
+  plt.plot(total_val_loss, label='validation_accuracy')
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label='Start Fine Tuning')
+  plt.legend(loc='upper right')
+  plt.xlabel('Epochs')
+  plt.title('Training & Validation Loss')
+  
